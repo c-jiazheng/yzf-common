6465e81d7894b383834049f22e51e2fc7e34e285
chenjiazheng <c-jiazheng@163.com>
2019-04-24 19:07:29 +0800

    initFlag

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/c-jiazheng/yzf-common
 
 go 1.12
+
+require (
+	github.com/alecthomas/template v0.0.0-20160405071501-a0175ee3bccc // indirect
+	github.com/alecthomas/units v0.0.0-20151022065526-2efee857e7cf // indirect
+	github.com/peggypig/nacos-go v0.0.0-20190401050939-2a8e7d04f4d5
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6
+)
--- kingpinFlag/flag.go
+++ kingpinFlag/flag.go
@@ -3,25 +3,25 @@ package kingpinFlag
 import "gopkg.in/alecthomas/kingpin.v2"
 
 //添加参数定义
-func InitFlag() (nacosHost, nacosDiscoverClient, nodeType, serviceName string) {
+func InitFlag() (nacosHost, nacosDiscoverClient, nodeType, serviceName *string) {
 
 	//nacos注册中心地址
-	nacosHost = *kingpin.Flag(
+	nacosHost = kingpin.Flag(
 		"nacos.host",
 		"Nacos Host default http://172.24.28.2:8848",
 	).Default("172.24.28.2:8848").Envar("NACOS_HOST").String()
 	//注册到nacos的服务地址
-	nacosDiscoverClient = *kingpin.Flag(
+	nacosDiscoverClient = kingpin.Flag(
 		"nacos.discoverClient.ip",
 		"Registry Nacos client ip address.",
 	).Default("").Envar("NACOS_DISCOVER_CLIENT_IP").String()
 	//节点类型
-	nodeType = *kingpin.Flag(
+	nodeType = kingpin.Flag(
 		"nacos.nodeType",
 		"Registry Nacos meteData node_type.",
 	).Default("exporter").String()
 	//服务名称
-	serviceName = *kingpin.Flag(
+	serviceName = kingpin.Flag(
 		"nacos.serviceName",
 		"Registry Nacos serviceName.",
 	).Default("node-exporter").String()
