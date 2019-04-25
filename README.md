0bdd081b8f3646bbfdb88cb6dd4a5b63d76cdbcc
chenjiazheng <c-jiazheng@163.com>
2019-04-25 09:39:26 +0800

    配置参数

--- README.md
+++ README.md
@@ -4,4 +4,12 @@
  [![Coverage Status](https://coveralls.io/repos/github/oliver006/redis_exporter/badge.svg?branch=master)](https://coveralls.io/github/oliver006/redis_exporter?branch=master) 
  [![codecov](https://codecov.io/gh/oliver006/redis_exporter/branch/master/graph/badge.svg)](https://codecov.io/gh/oliver006/redis_exporter)
  
- ### 
+ ### 参数
+ - nacos.server-address nacos 注册中心地址\
+ 默认 172.24.28.2:8848,可以设置evn NACOS_SERVER_ADDRESS
+ - nacos.discover-client-address \
+ 注册到nacos的服务的客户端地址，evn NACOS_DISCOVER_CLIENT_ADDRESS,如果不设置则取第一个网卡地址。
+ - nacos.node-type \
+ 注册到nacos的节点类型 默认为：exporter
+ - nacos.service-name \
+ 注册到nacos的服务明
--- kingpinFlag/flag.go
+++ kingpinFlag/flag.go
@@ -3,28 +3,28 @@ package kingpinFlag
 import "gopkg.in/alecthomas/kingpin.v2"
 
 //添加参数定义
-func InitFlag() (nacosHost, nacosDiscoverClient, nodeType, serviceName *string) {
+func InitFlag(serviceName string) (nacosHost, nacosDiscoverClient, nodeType *string) {
 
 	//nacos注册中心地址
 	nacosHost = kingpin.Flag(
-		"nacos.host",
-		"Nacos Host default http://172.24.28.2:8848",
-	).Default("172.24.28.2:8848").Envar("NACOS_HOST").String()
+		"nacos.server-address",
+		"Nacos server address default http://172.24.28.2:8848",
+	).Default("172.24.28.2:8848").Envar("NACOS_SERVER_ADDRESS").String()
 	//注册到nacos的服务地址
 	nacosDiscoverClient = kingpin.Flag(
-		"nacos.discoverClient.ip",
-		"Registry Nacos client ip address.",
-	).Default("").Envar("NACOS_DISCOVER_CLIENT_IP").String()
+		"nacos.discover-client-address",
+		"Registry Nacos client address.",
+	).Default("").Envar("NACOS_DISCOVER_CLIENT_ADDRESS").String()
 	//节点类型
 	nodeType = kingpin.Flag(
-		"nacos.nodeType",
+		"nacos.node-type",
 		"Registry Nacos meteData node_type.",
 	).Default("exporter").String()
 	//服务名称
-	serviceName = kingpin.Flag(
-		"nacos.serviceName",
+	kingpin.Flag(
+		"nacos.service-name",
 		"Registry Nacos serviceName.",
-	).Default("node-exporter").String()
+	).Default(serviceName).String()
 
 	return
 }
